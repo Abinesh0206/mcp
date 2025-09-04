267349a0af0dc742298337788a381d0622374fb7
Abinesh0206 <abinesh.n@cloudmasa.com>
2025-09-04 11:12:34 +0530

    Update app.py

--- files/app.py
+++ files/app.py
@@ -9,7 +9,7 @@ import google.generativeai as genai
 load_dotenv()
 
 MCP_SERVER_URL = os.getenv("MCP_SERVER_URL", "http://13.221.252.52:3000/mcp")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyA-iOGmYUxW000Nk6ORFFopi3cJE7J8wA4")
 GEMINI_MODEL = os.getenv("GEMINI_MODEL", "gemini-1.5-flash")
 K8S_MCP_TOKEN = os.getenv("K8S_MCP_TOKEN", "")
 
