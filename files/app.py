29b66a67eb9c86c48be76ac4d3d0d01814300914
Abinesh0206 <abinesh.n@cloudmasa.com>
2025-09-13 17:19:14 +0530

    Update app.py

--- files/app.py
+++ files/app.py
@@ -10,7 +10,7 @@ from datetime import datetime, timezone
 # ---------------- CONFIG ----------------
 load_dotenv()
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyA-iOGmYUxW000Nk6ORFFopi3cJE7J8wA4")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyC9wiw7fC2StEswOaINsoOw4Ip4n-9IDa4")
 GEMINI_MODEL = os.getenv("GEMINI_MODEL", "gemini-1.5-flash")
 
 if GEMINI_API_KEY:
