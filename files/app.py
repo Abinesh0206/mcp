4eab18e23d40b1936a649615dd21a21dfcac1274
Abinesh0206 <abinesh.n@cloudmasa.com>
2025-09-17 16:43:20 +0530

    Update app.py

--- files/app.py
+++ files/app.py
@@ -10,7 +10,7 @@ from typing import Optional, Dict, Any
 
 # ---------------- CONFIG ----------------
 load_dotenv()
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyAkqKd3Hc60Qf6N_3ZYj1eu_GtFzkMmMVQ")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyC7iRO4NnyQz144aEc6RiVUNzjL9C051V8")
 GEMINI_MODEL = os.getenv("GEMINI_MODEL", "gemini-1.5-flash")
 
 GEMINI_AVAILABLE = False
