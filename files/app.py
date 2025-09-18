642e17371bc68bbd541c341068aa7468966d978d
Abinesh0206 <abinesh.n@cloudmasa.com>
2025-09-18 23:43:32 +0530

    Update app.py

--- files/app.py
+++ files/app.py
@@ -15,7 +15,7 @@ import google.generativeai as genai
 # ================= CONFIG =================
 load_dotenv()
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyBYRBa7dQ5atjlHk7e3IOdZBdo6OOcn2Pk")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyC7iRO4NnyQz144aEc6RiVUNzjL9C051V8")
 GEMINI_MODEL = os.getenv("GEMINI_MODEL", "gemini-1.5-flash")
 
 GEMINI_AVAILABLE = False
